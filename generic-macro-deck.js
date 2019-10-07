bdb22eb727c4031ee616ebf17d653d3aa2bca09d
Alice Gaudon <alice@gaudon.pro>
2019-10-07 13:33:10 +0200

    Allow only one instance of the program to run at the same time

--- generic-macro-deck.js
+++ generic-macro-deck.js
@@ -1,3 +1,10 @@
 #!/bin/electron
-require = require("esm")(module);
-module.exports = require("./src/main.js");
+const SingleInstance = require('single-instance');
+const lock = new SingleInstance('generic-macro-deck');
+lock.lock().then(() => {
+    require = require("esm")(module);
+    module.exports = require("./src/main.js");
+}).catch(error => {
+    console.error(error);
+    process.exit(0);
+});
